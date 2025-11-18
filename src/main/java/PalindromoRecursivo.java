7d9458b7f1b48b2b06cd9765897a0f95731389e4
juliana.forbici <juliana.forbici@senior.com.br>
2025-11-18 17:09:50 -0300

    Exercicio de calculadora simples (sem testes). Feito exemplos de recursividade para aprendizado
    (cherry picked from commit fb4e9ea9c70e3570c60dd896c954e1f895d5d3e7)

---
+++ src/main/java/PalindromoRecursivo.java
@@ -0,0 +1,22 @@
+public class PalindromoRecursivo {
+    public static boolean ehPalindromo(String texto) {
+        texto = texto.replace(" ", "").toLowerCase();
+        if (texto.length() <= 1) {
+            return true;
+        }
+
+        char primeira = texto.charAt(0);
+        char ultima = texto.charAt(texto.length() - 1);
+
+        if (primeira != ultima) {
+            return false;
+        }
+
+        String meio = texto.substring(1, texto.length() - 1);
+        return ehPalindromo(meio);
+    }
+    public static void main(String[] args) {
+        System.out.println(ehPalindromo("arara"));
+        System.out.println(ehPalindromo("java"));
+    }
+}
