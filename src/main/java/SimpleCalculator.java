c1f14b2ae51f5d93e5b1f652bd3ac145c82b56e9
juliana.forbici <juliana.forbici@senior.com.br>
2025-11-18 15:31:58 -0300

    Calculadora simples que realiza a soma e a divisão e teste em Junit, teste realiza a exceção e a validação da divisão e a soma.

---
+++ src/main/java/SimpleCalculator.java
@@ -0,0 +1,8 @@
+public class SimpleCalculator {
+    public int add(int numberA, int numberB) {
+        return numberA + numberB;
+    }
+    public int divide(int a, int b) {
+        return a / b;
+    }
+}
