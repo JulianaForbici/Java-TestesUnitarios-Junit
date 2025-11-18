bd9ee61ef7afb617e503429a83160da96a10c1c9
juliana.forbici <juliana.forbici@senior.com.br>
2025-11-18 16:15:53 -0300

    Realizado teste de string e utilizado o teste parametrizado e o valuesource para aprendizado

---
+++ src/test/java/StringsTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class StringsTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void isBlank_ShouldReturnTrueForNullOrBlankStrings(String input) {
+        assertTrue(Strings.isBlank(input));
+    }
+}
