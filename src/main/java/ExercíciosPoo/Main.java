0c94b01e3272b09ae2be5ab96e3650cff5790e49
juliana.forbici <juliana.forbici@senior.com.br>
2025-11-24 17:31:36 -0300

    feat: adicionado exercício 1 de programação orientada a objetos completo

--- src/main/java/ExercíciosPoo/Main.java
+++ src/main/java/ExercíciosPoo/Main.java
@@ -1,4 +1,14 @@
 package ExercíciosPoo;
 
 public class Main {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa("Juliana Ju", "123.456.789-00");
+        Carro c = new Carro("Volkswagen", "Passat", "Amarelo queimado", 1974 , 200.0, p);
+
+        System.out.println(c);
+        System.out.println("Proprietário do carro: " + c.getProprietario());
+        System.out.println("Tempo de uso do carro: " + c.calcularTempoDeUsoEmAnos());
+        System.out.printf("Valor de revenda: R$ %.2f%n", c.calcularValorRevenda());
+        System.out.printf("IPVA: R$ %.2f%n", c.calcularIpva());
+    }
 }
--- src/main/java/ExercíciosPoo/Pessoa.java
+++ src/main/java/ExercíciosPoo/Pessoa.java
@@ -5,18 +5,28 @@ public class Pessoa {
     private String nome;
     private String cpf;
 
-    public String getCpf() {
-        return cpf;
-    }
-
-    public  void setCpf(String cpf) {
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
         this.cpf = cpf;
     }
 
+    // getters e setters
     public String getNome() {
         return nome;
     }
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " (" + cpf + ")";
     }
 }
